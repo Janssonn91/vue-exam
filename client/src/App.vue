2d7bab9341abaad056bb6c9a22fb66252d4cfc96
Niklas J <Niklas.Jansson91@hotmail.com>
2019-01-11 15:52:05 +0100

    Adds style to homepage, href working, added comments

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Auth for Newbs</a>
+      <a class="navbar-brand" href="/">Login and auth for beginners</a>
     </nav>
+    <!-- Open what compnents the router-view match.
+    Example: Home component which is startpage will come here beneeth.
+    Inside Home, there are router-links for login/signup,
+    those will open those component when "activated".-->
     <router-view class="container pt-2" />
   </div>
 </template>
 
 <style>
+
+.container {
+  max-width: 960px!important;
+}
 
 </style>
